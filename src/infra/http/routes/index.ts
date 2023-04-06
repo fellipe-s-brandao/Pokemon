91ce2e661e5319c4a9e16e4b73ee94c3cac8fd17
Fellipe_Brandao <fellipesilverio31@hotmail.com>
2023-04-06 12:42:52 -0300

    fix

--- src/infra/http/routes/index.ts
+++ src/infra/http/routes/index.ts
@@ -4,13 +4,6 @@ import { batalharRoutes } from './batlhar.routes';
 
 const router = Router();
 
-router.use("/", (request: Request, response: Response) => {
-    response.json({
-            message: 'api-pokemon',
-            author: 'Fellipe Silvério Brandão'
-        })
-});
-
 router.use("/pokemons", pokemonsRoutes);
 router.use("/batalhar", batalharRoutes);
 
